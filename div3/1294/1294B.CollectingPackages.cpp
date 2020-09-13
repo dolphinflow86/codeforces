7dfabf598b5d8373da47801063c81917b0897550
Sean Choi <sean@Sean-Choiui-MacBookPro.local>
2020-09-13 23:35:01 +0900

    Solved Round 1294 B

--- div3/1294/1294B.CollectingPackages.cpp
+++ div3/1294/1294B.CollectingPackages.cpp
@@ -1,27 +1,56 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
 	int t;
 	cin >> t;
-	
+
 	while(t--) {
 		int n;
 		cin >> n;
-		vector<pair<int, int>> list;
+		vector<pair<int, int>> points;
+		vector<char> results;
 		int rx = 0, ry = 0;
-		
+		int ix, iy;
+
 		while(n--) {
-			int x, y;
-			cin >> x >> y;
-			list.emplace_back(make_pair(x,y));
+			cin >> ix >> iy;
+			points.emplace_back(make_pair(iy,ix));
 		}
-		
-		
+
+		sort(points.begin(), points.end());
+
+		bool isPossible = true;
+		for(int i = 0; i < points.size(); ++i) {
+			int y = points[i].first;
+			int x = points[i].second;
+
+			if(x < rx) {
+				isPossible = false;
+				break;
+			}
+
+			int yDist = y - ry;
+			int xDist = x - rx;
+			ry = y;
+			rx = x;
+			while(xDist--) results.emplace_back('R');
+			while(yDist--) results.emplace_back('U');
+		}	
+
+		if(!isPossible) {
+			cout << "NO\n";
+			continue;
+		} else {
+			cout << "YES\n";
+			for(int i = 0; i < results.size(); ++i) {
+				cout << results[i];
+			}
+		}
+		cout << "\n";
 	}
 	return 0;
 }
-
-
